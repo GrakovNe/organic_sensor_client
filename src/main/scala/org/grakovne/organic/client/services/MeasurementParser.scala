f2c29a0a5ddc4b29b16a8e6fb683bb99d8004732
grakovne <grakovne@gmail.com>
2021-07-08 19:30:36 +0300

    [CRITICAL] skip failed measurements

--- src/main/scala/org/grakovne/organic/client/services/MeasurementParser.scala
+++ src/main/scala/org/grakovne/organic/client/services/MeasurementParser.scala
@@ -9,12 +9,15 @@ import org.slf4j.LoggerFactory
 class MeasurementParser(configuration: OrganicSensorClientConfiguration) {
 
   private val log: Logger = LoggerFactory.getLogger(this.getClass)
+  private var debounceCounter = 0
 
   private def parseMeasurement(rawMeasurement: String, builder: MeasurementBuilder): Unit = {
     val data = rawMeasurement.split(configuration.measurementSeparator).toList
 
     val name = data.headOption.map(_.trim)
     val value = data.lastOption.map(_.trim)
+
+    log.info((name, value).toString())
 
     name.map {
       case configuration.measurementKeys.tvocKey => builder.insertTvoc(value.flatMap(_.toDoubleOption))
@@ -27,6 +30,19 @@ class MeasurementParser(configuration: OrganicSensorClientConfiguration) {
   }
 
   def parse(raw: String): Option[Measurement] = {
+
+    isDataValid(raw) match {
+      case true => reduceFromDebounce()
+      case false => raiseToDebounce()
+    }
+
+    isDebounceRaised() match {
+      case true => None
+      case false => parseData(raw)
+    }
+  }
+
+  private def parseData(raw: String): Option[Measurement] = {
     val measurementBuilder = new MeasurementBuilder()
 
     raw
@@ -35,8 +51,18 @@ class MeasurementParser(configuration: OrganicSensorClientConfiguration) {
       .split(configuration.measurementsDelimiter)
       .foreach(parseMeasurement(_, measurementBuilder))
 
+    log.debug(s"got measurement: ${measurementBuilder.build()}")
     measurementBuilder.build()
   }
+
+  private def raiseToDebounce(): Unit = debounceCounter = 12
+
+  private def reduceFromDebounce(): Unit =
+    if (debounceCounter < 1) debounceCounter = 0 else debounceCounter = debounceCounter - 1
+
+  private def isDebounceRaised(): Boolean = debounceCounter != 0
+
+  private def isDataValid(raw: String) = !raw.toLowerCase().contains("nan")
 }
 
 object MeasurementParser {
